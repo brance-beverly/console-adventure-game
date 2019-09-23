c60fbf3c6f69acf103fa3cc045ea4cb55d9bb73e
BranceA <brancealexander@gmail.com>
2019-09-23 11:37:02 -0500

    player can enter name

--- src/ConsoleAdventureGame.java
+++ src/ConsoleAdventureGame.java
@@ -5,15 +5,20 @@ public class ConsoleAdventureGame {
 
     public static void main(String[] args) {
 
-        wantToPlay();
-        String playerName;
+        boolean readyToPlay = wantToPlay();
+        String playerName = "";
 
+        if(readyToPlay){
+            playerName = whatIsYourName();
+        }
+
+        System.out.println(playerName);
 
     }
 
 //    this asks the player if they are ready at the beggining
 
-    public static void wantToPlay(){
+    public static boolean wantToPlay(){
         Scanner input = new Scanner(System.in);
         System.out.println("Are you ready to start your Canadian adventure, Eh? Y/N");
         String answer = input.nextLine();
@@ -21,18 +26,21 @@ public class ConsoleAdventureGame {
 //        while (answer.equalsIgnoreCase("n"))
         if(answer.equalsIgnoreCase("n")){
             System.out.println("Soory to hear that.");
-        } else if (answer.equalsIgnoreCase("y")){
-            System.out.println("Awesome");
-        } else {
+            wantToPlay();
+        } else if (!answer.equalsIgnoreCase("n") && !answer.equalsIgnoreCase("y")){
             System.out.println("I don't know what that means. Try again please.");
             wantToPlay();
+        } else {
+            System.out.println("Awesome");
         }
+
+        return true;
     }
 
     public static String whatIsYourName(){
         Scanner input = new Scanner(System.in);
         System.out.println("Would you enter your name please?");
-       String playerName = input.nextLine();
+        String playerName = input.nextLine();
 
         return playerName;
     }
